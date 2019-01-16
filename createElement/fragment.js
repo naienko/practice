0fe796d98fa457b06b1887e2e5099325ff8965e4
Panya Farnette <naienko@gmail.com>
2019-01-16 13:45:53 -0600

    document fragments

--- createElement/fragment.js
+++ createElement/fragment.js
@@ -1,5 +1,8 @@
 const messages = document.querySelector("#messages");
 
+const fragment = document.createDocumentFragment();
+
+
 const msgBuilder = (...info) => {
     const msgItem = document.createElement('section');
     msgItem.className = "message";
@@ -7,11 +10,15 @@ const msgBuilder = (...info) => {
         spanWrapper.className = info[0];
         spanWrapper.textContent = `${info[0]}: ${info[1]}`;
     msgItem.appendChild(spanWrapper);
-    messages.appendChild(msgItem);
+    fragment.appendChild(msgItem);
 }
+
+//how to simplify stacks of createElement
 
 for (const msg of messageList) {
     let msgConversation = "";
     msgConversation += msgBuilder(msg.speaker, msg.message);
     console.log = msgConversation;
 }
+
+messages.appendChild(fragment);
