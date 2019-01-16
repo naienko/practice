6efb767cf77fe0141d89f8f4d0a00a4b0442725e
Panya Farnette <naienko@gmail.com>
2019-01-16 13:08:55 -0600

    built the message list in html

--- createElement/main.js
+++ createElement/main.js
@@ -1,22 +1,18 @@
 const messages = document.querySelector("#messages");
 
-//arguments = html element, data inside the element, styles
-const elementBuilder = (...arguments) => {
-    return `<${arguments[0]} class="${arguments[2]}">${arguments[1]}</${arguments[0]}>`;
+const msgBuilder = (...info) => {
+    const msgItem = document.createElement('section');
+    msgItem.className = "message";
+        const spanWrapper = document.createElement('span');
+        spanWrapper.className = info[0];
+        spanWrapper.textContent = `${info[0]}: ${info[1]}`;
+    msgItem.appendChild(spanWrapper);
+//    msgItem.textContent = `${info[0]}: ${info[1]}`;
+    messages.appendChild(msgItem);
 }
 
-// Create an unordered list element
-// const list = document.createElement('ul')
-
-// Create a list item for our list
-// const listItem = document.createElement('li')
-// listItem.className = "president"
-// listItem.textContent = "George Bush"
-
-// Put the list item on the unordered list
-// list.appendChild(listItem)
-
-const msgItem = document.createElement('section');
-msgItem.className = "message";
-msgItem.textContent = "a message goes here"
-messages.appendChild(msgItem);
+for (const msg of messageList) {
+    let msgConversation = "";
+    msgConversation += msgBuilder(msg.speaker, msg.message);
+    console.log = msgConversation;
+}
---
+++ createElement/messages.js
@@ -0,0 +1,44 @@
+const messageList = [
+    {
+        speaker: "me",
+        message: "*tries to get off the sofa and gets tangled in palazzo pants, faceplanting on the floor*"
+    },
+    {
+        speaker: "husban",
+        message: "*tries not to laugh and fails. a lot*"
+    },
+    {
+        speaker: "me",
+        message: "*unhurt, in mock outrage* I'm going to hit you with a sword!"
+    },
+    {
+        speaker: "husban",
+        message: "*sloooowly backing away*"
+    },
+    {
+        speaker: "me",
+        message: "*scramble for the rubber sword*"
+    },
+    {
+        speaker: "husban",
+        message: "*hurrying down the hall*"
+    },
+    {
+        speaker: "me",
+        message: "*chasing* Don't you waggle your pantaloons at me!"
+    },
+    {
+        speaker: "us",
+        message: "*collapsing on the bed laughing ourselves crosseyed*"
+    },
+    {
+        speaker: "helios",
+        message: "*bites the sword*"
+    }
+];
+
+// husban: *hurrying down the hall*
+// me: *chasing* Don't you waggle your pantaloons at me!
+// us: *collapsing on the bed laughing ourselves crosseyed*
+
+// helios: *bites the sword*
